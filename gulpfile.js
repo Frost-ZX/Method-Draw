35251292ca326f727a2c640688e2575858818889
Frost-ZX <30585462+Frost-ZX@users.noreply.github.com>
2020-12-10 14:00:38 +0800

    更改 docs 文件结构

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,20 @@ const minify = require('gulp-minify');
 gulp.task('css', function () {
   return gulp.src('src/css/*.css')
     .pipe(concat('all.css'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./dist/css/'));
 });
 
 gulp.task('js', function () {
   return gulp.src(['src/js/*.js', 'src/lib/*.js']) 
     .pipe(concat('all.js'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./dist/js/'));
 });
 
 gulp.task('index', function () {
     return gulp.src('src/*.html')
         .pipe(useref())
         .pipe(cachebust({type: 'timestamp'}))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('./dist/'));
 });
 
 // no service worker implemented yet
@@ -38,33 +38,33 @@ gulp.task('manifest', function(){
 
 gulp.task('images', function(){
     return gulp.src(['src/images/**/*'])
-    .pipe(gulp.dest('dist/images'));
+    .pipe(gulp.dest('./dist/images/'));
 });
 
 gulp.task('fonts', function(){
     return gulp.src(['src/css/font-files/**/*'])
-    .pipe(gulp.dest('dist/font-files'));
+    .pipe(gulp.dest('./dist/css/font-files/'));
 });
 
 gulp.task('extensions', function(){
     return gulp.src(['src/extensions/**/*'])
-    .pipe(gulp.dest('dist/extensions'));
+    .pipe(gulp.dest('./dist/extensions/'));
 });
 
 gulp.task('shapelib', function(){
     return gulp.src(['src/shapelib/**/*'])
-    .pipe(gulp.dest('dist/shapelib'));
+    .pipe(gulp.dest('./dist/shapelib/'));
 });
 
 gulp.task('canvg', function(){
     return gulp.src(['src/js/lib/canvg.js', 'src/js/lib/rgbcolor.js'])
-    .pipe(gulp.dest('dist/js/lib'));
+    .pipe(gulp.dest('./dist/js/lib/'));
 });
 
 gulp.task('default', 
     gulp.series(
-        'css', 
-        'js', 
+        // 'css', 
+        // 'js', 
         'index', 
         'manifest',
         'images',
